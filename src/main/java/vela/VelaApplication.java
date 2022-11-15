4a347fcd78b008249a632b7621ccbeecb4bf0b66
kochd <kocdoc18@htl.kaindorf.at>
2022-11-15 13:47:48 +0100

    Initial Commit

---
+++ src/main/java/vela/VelaApplication.java
@@ -0,0 +1,13 @@
+package vela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VelaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VelaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vela/VelaApplicationTests.java
@@ -0,0 +1,13 @@
+package vela;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VelaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
