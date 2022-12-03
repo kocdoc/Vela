19a0ed9a1467c4ea4140f12713509fa084d36f21
mucmac18 <mucmac18@htl-kaindorf.at>
2022-12-03 19:04:26 +0100

    - Vue Frontend Basic Setup

---
+++ src/frontend/src/views/HomeView.vue
@@ -0,0 +1,29 @@
+<script>
+// @ is an alias to /src
+export default {
+  name: 'HomeView',
+  data () {
+    return {
+      test: []
+    }
+  },
+  methods: {
+    testMethod () {
+      fetch('/api/taskmanager/getTasks?username=admin')
+        .then((response) => { return response.json() })
+        .then(data => {
+          data.forEach(task => {
+            console.log(task)
+          })
+        })
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="home">
+    <h1>Test</h1>
+    <button @click="testMethod"></button>
+  </div>
+</template>
--- src/main/java/vela/resource/TaskManagerResource.java
+++ src/main/java/vela/resource/TaskManagerResource.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @Slf4j
 @RestController
-@RequestMapping("/taskmanager")
+@RequestMapping("/api/taskmanager")
 public class TaskManagerResource {
 
     private DBConnection instance = DBConnection.getInstance();
