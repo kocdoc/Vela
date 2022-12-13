68d93cd27bf6fbc05fd5842174cb91526e72e727
jartoc18 <jartoc18@htl-kaindorf.at>
2022-12-13 14:55:24 +0100

    added ProfileResource.getUserData()

---
+++ src/main/java/vela/resource/ProfileResource.java
@@ -0,0 +1,38 @@
+package vela.resource;
+
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import vela.database.DBConnection;
+import vela.jwt.JWTNeededFilter;
+import vela.pojos.User;
+
+import java.text.ParseException;
+
+@RestController
+@RequestMapping("/api/profile")
+@Slf4j
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class ProfileResource {
+
+    DBConnection dbConnection = DBConnection.getInstance();
+
+    @PostMapping("/getUserData")
+    public ResponseEntity getUserData(@RequestParam(value="user") String jwt){
+        try {
+            User user = dbConnection.getUserByUsername(JWTNeededFilter.getUsername(jwt));
+            User user2 = new User(user.getUsername(), user.getFirstname(), user.getLastname(), user.getEmail(), user.getPassword());
+            return ResponseEntity.ok(user2);
+        } catch (ParseException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("jwt not valid");
+        }
+    }
+}
